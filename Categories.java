5af52b7645ec53e6f60180b45b2a9a9dad8fea37
kimherdz <98179334+kimherdz@users.noreply.github.com>
2023-07-11 13:52:23 +0000

    esquema

--- Categories.java
+++ Categories.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Categories{
+    Hashtable<String, LinkedList<String>> three = new Hashtable<>();
+    int counter = 0;
+
+    public void classify(String input) {
+        
+    }
+
+    public void printPhones() {
+        
+    }
+
+    public void printDates() {
+        
+    }
+
+    public void countByCategory() {
+        
+    }
+}
+
 
